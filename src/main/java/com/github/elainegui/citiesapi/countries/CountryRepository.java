85e61ea1dcea0db8bb604ca2d0bd5341cddef0f4
elainegui <elainegui@gmail.com>
2021-11-25 11:40:45 +0000

    first commit

--- src/main/java/com/github/elainegui/citiesapi/countries/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.elainegui.citiesapi.countries;
-
-import com.github.elainegui.citiesapi.countries.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository <Country,Long> {
-}
